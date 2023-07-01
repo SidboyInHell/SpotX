62ad7d4a640d085cb76944e60be13cda54daacf7
SidboyInHell <122257501+SidboyInHell@users.noreply.github.com>
2023-07-01 13:19:06 +0530

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -267,7 +267,7 @@ $country = [System.Globalization.RegionInfo]::CurrentRegion.EnglishName
 function CallLang($clg) {
 
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    $urlLang = "https://raw.githubusercontent.com/amd64fox/SpotX/main/scripts/installer-lang/$clg.ps1"
+    $urlLang = "https://cdn.jsdelivr.net/gh/amd64fox/SpotX/scripts/installer-lang/$clg.ps1"
     $ProgressPreference = 'SilentlyContinue'
     
     try {
@@ -289,7 +289,7 @@ $lang = CallLang -clg $langCode
 # Set variable 'ru'.
 if ($langCode -eq 'ru') { 
     $ru = $true
-    $urlru = "https://raw.githubusercontent.com/amd64fox/SpotX/main/patches/Augmented%20translation/ru.json"
+    $urlru = "https://cdn.jsdelivr.net/gh/amd64fox/SpotX/patches/Augmented%20translation/ru.json"
     $webjsonru = (Invoke-WebRequest -useb -Uri $urlru).Content | ConvertFrom-Json
 }
 
@@ -839,7 +839,7 @@ if ($ch -eq 'n') {
 
 $ch = $null
 
-$url = "https://raw.githubusercontent.com/amd64fox/SpotX/main/patches/patches.json"
+$url = "https://cdn.jsdelivr.net/gh/amd64fox/SpotX/patches/patches.json"
 $retries = 0
 
 while ($retries -lt 3) {
